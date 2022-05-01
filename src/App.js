6c5b40b6b436ad3563e0495a4f200b2b86728688
h4uu11 <h4uu11@gmail.com>
2022-05-02 00:21:35 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,33 @@ import { movieAction } from "../redux/actions/movieAction";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { upcomingMovies, popularTvs } = useSelector(
-    (state) => state.movieReducer
-  );
+  const { popularTvs } = useSelector((state) => state.movieReducer);
 
   useEffect(() => {
     dispatch(movieAction.movieAll());
     dispatch(movieAction.tvAll());
   }, [dispatch]);
-  console.log(popularTvs);
+
   return (
+    /* https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/1uegR4uAxRxiMyX4nQnpzbXhrTw.jpg */
+    /* https://www.themoviedb.org/t/p/w600_and_h900_bestv2/h9fjtMeoaI3LrW8avMZoJPoniLZ.jpg */
     <div>
-      {/* https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/1uegR4uAxRxiMyX4nQnpzbXhrTw.jpg
-        https://www.themoviedb.org/t/p/w600_and_h900_bestv2/h9fjtMeoaI3LrW8avMZoJPoniLZ.jpg */}
+      <div>
+        <img
+          src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${
+            popularTvs.results && popularTvs.results[0].backdrop_path
+          }`}
+          alt=""
+        />
+      </div>
+      {popularTvs.results &&
+        popularTvs.results.map((item, index) => (
+          <img
+            key={index}
+            src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${item.poster_path}`}
+            alt=""
+          />
+        ))}
     </div>
   );
 };
